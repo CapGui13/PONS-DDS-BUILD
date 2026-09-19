dc7c7b53d409f22db817d9636c666a29ddda9daa
CapGui13 <guillaume_capron@yahoo.fr>
2026-09-19 16:45:03 +0200

    MANIEMENTS V6.2: narrow honor exhaustion to split observed honors

--- maniements-v5/product/v6/test_honor_exhaustion_v62.py
+++ maniements-v5/product/v6/test_honor_exhaustion_v62.py
@@ -24,7 +24,7 @@ for item in comp['program']:
     # still needing both tricks, two of the three honors above T have already
     # been forced out. Preserve T by ducking another small card.
     if k[0]==2 and k[1]=='response' and k[4]==0 and k[5]=='TA' and k[6]=='':
-        item['rules'].insert(0,{'if':[['seen_KQJ',2]],'action':'LOW'})
+        item['rules'].insert(0,{'if':[['seen_KQJ',2],['west_KQJ',1],['east_KQJ',1]],'action':'LOW'})
         patched+=1
 
 after,ma=sem.evaluate_program(eng,north,south,target,comp['program'])
